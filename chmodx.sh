1d55bb660250b638baa8208fd0ad721d853eb90e
Artour Konstantinov <artoukons@gmail.com>
2020-08-11 21:24:01 -0400

    fixed chmodx.sh script.

--- chmodx.sh
+++ chmodx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-f=$( ls -al | grep ^-..-..-..-.*\.sh$ | cut -d ' ' -f11 | rofi -dmenu -p chmod+x )
+f=$( ls -al | grep ^-..-..-..-.*\.sh$ | rev | cut -d ' ' -f1 | rev | rofi -dmenu -p chmod+x )
 if [ -z "$f" ]; then
 	exit
 fi
