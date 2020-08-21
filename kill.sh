00b39758cf4404fb71d2bea5635e2dd289720da3
Artour Konstantinov <artoukons@gmail.com>
2020-08-20 21:10:16 -0400

    added kill.sh script select process to kill with rofi or select to kill all killable processess must be listed in the "p" array

---
+++ kill.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+q=""
+declare -a p=("Discord" "zoom" "geary")
+
+for c in "${p[@]}"
+do
+	x=$(pidof $c) 
+	if [ -n "$x" ]; then
+		q="$q $c"
+	fi
+done
+
+#nothing to kill
+if [ -z "$q" ]; then
+	exit
+fi
+
+i=$(echo -e "All$q" | rofi -i -sep ' ' -dmenu -p kill)
+
+if [ -z "$i" ]; then
+	exit
+#elif [ $i == "None" ];then
+#	exit
+elif [ $i == "All" ];then
+	killall $q
+else
+	killall $i
+fi
